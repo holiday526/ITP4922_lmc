df3438c7a14a0355bcf90d686fdd9ca2baaf0d77
holiday526 <48387601+holiday526@users.noreply.github.com>
2020-07-03 18:42:31 +0800

    temp setting
    Set routing method
    project structure
    bootstrap implemented

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
+<!doctype html>
+<html lang="en">
+<?php require_once 'layout/head.php'; ?>
+<body>
+<?php require_once 'layout/navbar.php'; ?>
+<?php require_once 'layout/header.php'; ?>
 <?php
+if (isset($_GET['route'])){
+    $route = $_GET['route'];
+    switch ($route) {
+        case 'login': $page = 'login'; break;
+        case 'cart': $page = 'cart'; break;
+    }
+} else {
+    $page = 'home';
+}
 
-phpinfo();
+require_once "./page/$page.php";
+
+?>
+<?php require_once 'layout/footer.php'; ?>
+<?php require_once 'layout/footer-script.php'; ?>
+</body>
+</html>
