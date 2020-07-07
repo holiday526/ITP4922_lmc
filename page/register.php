b2e1a7c51bf5dcc2ebc3dd0f4f575db516a400a1
AkitoShing <samuelxx100799@gmail.com>
2020-07-07 19:35:50 +0800

    Update Catalog Add uid to register form

--- page/register.php
+++ page/register.php
@@ -6,14 +6,24 @@
             <input type="email" name="email" class="form-control" id="emailInput" placeholder="Enter email">
         </div>
         <div class="form-group">
+            <label for="userIdInput">User ID</label>
+            <input type="text" name="uid" class="form-control" id="userIdInput" placeholder="Enter User ID for login"
+                   pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{6,16}$">
+            <small id="passwordHelp" class="form-text text-muted">User ID Length must be 6 - 16 characters.</small>
+        </div>
+        <div class="form-group">
             <label for="passwordInput">Password</label>
-            <input type="password" name="password" class="form-control" id="passwordInput" placeholder="Enter password" aria-describedby="passwordHelp" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$">
+            <input type="password" name="password" class="form-control" id="passwordInput" placeholder="Enter password"
+                   aria-describedby="passwordHelp" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$">
             <small id="passwordHelp" class="form-text text-muted">Password Length must be 8 - 20 characters.</small>
         </div>
         <div class="form-group">
             <label for="passwordInputConfirm">Confirm Password</label>
-            <input type="password" name="c_password" class="form-control" id="passwordInputConfirm" placeholder="Enter password" aria-describedby="passwordConfirmHelp" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$">
-            <small id="passwordConfirmHelp" class="form-text text-muted">Password Length must be 8 - 20 characters.</small>
+            <input type="password" name="c_password" class="form-control" id="passwordInputConfirm"
+                   placeholder="Enter password" aria-describedby="passwordConfirmHelp"
+                   pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$">
+            <small id="passwordConfirmHelp" class="form-text text-muted">Password Length must be 8 - 20
+                characters.</small>
         </div>
         <div class="form-group form-check">
             <input type="checkbox" class="form-check-input" id="corporateCustomerCheckbox" name="corporateCustomer">
@@ -29,8 +39,11 @@
         </div>
 
         <div class="form-group form-row">
-            <div class="col-4 mx-auto"><button type="submit" class="btn btn-success btn-md btn-block">Submit</button></div>
-            <div class="col-4 mx-auto"><a href="?route=login" class="btn btn-primary btn-md btn-block">Login existing</a></div>
+            <div class="col-4 mx-auto">
+                <button type="submit" class="btn btn-success btn-md btn-block">Submit</button>
+            </div>
+            <div class="col-4 mx-auto"><a href="?route=login" class="btn btn-primary btn-md btn-block">Login
+                    existing</a></div>
         </div>
     </form>
 </div>
