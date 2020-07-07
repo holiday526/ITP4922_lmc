260c14ac88a29ab6c886b2b664a62d1b2b8a3848
AkitoShing <samuelxx100799@gmail.com>
2020-07-07 19:47:55 +0800

    Update compare catalog Add compare route button

--- page/catalog.php
+++ page/catalog.php
@@ -1,5 +1,26 @@
 <?php
+session_start();
+
+if (isset($_GET['addcompare'])) {
+    if (count(array_filter($_SESSION['compareList'])) < 4) {
+        if (!in_array($_GET['addcompare'], $_SESSION['compareList'])) {
+            $_SESSION['compareList'][] = $_GET['addcompare'];
+            dd($_SESSION);
+        }
+    }
+    header('Location: ' . $_SERVER['HTTP_REFERER'] . '#car' . $_GET['addcompare']);
+} elseif (isset($_GET['delcompare'])) {
+    if (in_array($_GET['delcompare'], $_SESSION['compareList'])) {
+        if (($key = array_search($_GET['delcompare'], $_SESSION['compareList'])) !== false) {
+            unset($_SESSION['compareList'][$key]);
+        }
+        dd($_SESSION);
+    }
+    header('Location: ' . $_SERVER['HTTP_REFERER'] . '#car' . $_GET['delcompare']);
+}
+
 if (isset($_GET['carId'])) {
+//    Show the queried car (single car)
     $car = queryBuilderPrepare('cars',
         ['*'],
 //        ['cars.name as carName', 'customers.name as customerName', 'cars.id as carId'],
@@ -106,7 +127,7 @@ if (isset($_GET['carId'])) {
                         </div>
 
                         <div class="card-footer">
-                            <?=$car['name']?>
+                            <?= $car['name'] ?>
                         </div>
                     </div>
                 </div>
@@ -124,6 +145,7 @@ if (isset($_GET['carId'])) {
 
     <?php
 } else {
+//    Show all car available in database (Full catalog)
     $catalogString = '';
     if (isset($_GET['type'])) {
         switch ($_GET['type']) {
@@ -174,18 +196,29 @@ if (isset($_GET['carId'])) {
             <?php $carUrl = "/?route=catalog&carId=" . $car['carId'] ?>
             <?php $customerUrl = "/?route=profile&id=" . $car['customerId'] ?>
             <!-- Blog Post -->
-            <div class="card mb-4">
+            <div id="car<?= $car['carId'] ?>" class="card mb-4">
                 <div class="card-body">
                     <div class="row">
-                        <div class="col-lg-6">
+                        <div class="col-6">
                             <a href="<?= $carUrl ?>">
                                 <img class="img-fluid rounded" src="<?= $car['photoLocation'] ?>" alt="">
                             </a>
                         </div>
-                        <div class="col-lg-6">
+                        <div class="col-6">
                             <h2 class="card-title"><?= $car['carName'] ?></h2>
                             <p class="card-text"><?= $car['description'] ?></p>
-                            <a href="<?= $carUrl ?>" class="btn btn-primary">Read More &rarr;</a>
+                            <div class="d-flex flex-row py-1">
+                                <a href="<?= $carUrl ?>" class="btn btn-primary d-flex">Read More &rarr;</a>
+                                <?php if (in_array($car['carId'], $_SESSION['compareList'])) { ?>
+                                    <a class="btn btn-success d-flex px-2" href="/?route=catalog&delcompare=<?= $car['carId'] ?>">Remove from compare</a>
+                                <?php } else { ?>
+                                    <a class="btn btn-success d-flex px-2" href="/?route=catalog&addcompare=<?= $car['carId'] ?>">Add to compare</a>
+                                <?php } ?>
+                            </div>
+                            <div class="d-flex flex-row pt-1">
+                                <a href="<?= $carUrl ?>" class="btn btn-primary d-flex px-2">Make appointment</a>
+                                <a href="<?= $carUrl ?>" class="btn btn-primary d-flex px-2">Order Now!</a>
+                            </div>
                         </div>
                     </div>
                 </div>
--- page/compare.php
+++ page/compare.php
@@ -1,8 +1,9 @@
 <?php
-$carComparing = queryBuilderPrepare('cars', ['*'], ['id' => [1, 2, 3]]);
-//echo "<pre>";
-//var_dump($carComparing);
-//echo "</pre>";
+session_start();
+if (isset($_SESSION['compareList'])) {
+//    dd($_SESSION['compareList']);
+    $carComparing = queryBuilderPrepare('cars', ['*'], ['id' => $_SESSION['compareList']]);
+}
 ?>
 
 <div class="container pt-2">
@@ -11,45 +12,51 @@ $carComparing = queryBuilderPrepare('cars', ['*'], ['id' => [1, 2, 3]]);
         <div class="col-12">
             <div class="card-group">
                 <?php foreach ($carComparing as $car) { ?>
-                <div class="card text-left">
-                    <img class="card-img-top mx-auto d-block py-2"
-                         style="max-width: 100%; max-height: 100%;object-fit: scale-down"
-                         src="<?= $car['photoLocation']?>"
-                         height="200px" alt="Card image cap">
-                    <div class="card-body d-flex flex-column align-items-stretch mt-auto">
-                        <h3 class="card-title"><?= $car['name'] ?></h3>
-                        <p class="card-text"><?= $car["description"] ?></p>
-                    </div>
-
-                    <div class="card-body mt-auto">
-                        <h5 class="card-title">Pros</h5>
-                        <div>
-                            <?php foreach (explode("\n", $car['pros']) as $pros) { ?>
-                            <p class="card-text text-success"><?= $pros ?></p>
-                            <?php } ?>
+                    <div class="card text-left">
+                        <img class="card-img-top mx-auto d-block py-2"
+                             style="max-width: 100%; max-height: 100%;object-fit: scale-down"
+                             src="<?= $car['photoLocation'] ?>"
+                             height="200px" alt="Card image cap">
+                        <div class="card-body d-flex flex-column align-items-stretch mt-auto">
+                            <h3 class="card-title"><?= $car['name'] ?></h3>
+                            <p class="card-text"><?= $car["description"] ?></p>
                         </div>
-                    </div>
 
-                    <div class="card-body align-items-stretch mt-auto">
-                        <h5 class="card-title">Cons</h5>
-                        <div>
-                            <?php foreach (explode("\n", $car['cons']) as $cons) { ?>
-                            <p class="card-text text-danger"><?= $cons ?></p>
-                            <?php } ?>
+                        <div class="card-body mt-auto">
+                            <h5 class="card-title">Pros</h5>
+                            <div>
+                                <?php foreach (explode("\n", $car['pros']) as $pros) { ?>
+                                    <p class="card-text text-success"><?= $pros ?></p>
+                                <?php } ?>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="card-body mt-auto">
-                        <h5 class="card-title">Specifications</h5>
+                        <div class="card-body align-items-stretch mt-auto">
+                            <h5 class="card-title">Cons</h5>
+                            <div>
+                                <?php foreach (explode("\n", $car['cons']) as $cons) { ?>
+                                    <p class="card-text text-danger"><?= $cons ?></p>
+                                <?php } ?>
+                            </div>
+                        </div>
+
+                        <div class="card-body mt-auto">
+                            <h5 class="card-title">Specifications</h5>
                             <?php foreach (explode("\n", $car['specifications']) as $spec) { ?>
                                 <?php $item = explode(':', $spec) ?>
-                                    <h6 class="card-text"><?= $item[0] ?></h6>
-                                    <p class="card-text text-info"><?= $item[1] ?></p>
+                                <h6 class="card-text"><?= $item[0] ?></h6>
+                                <p class="card-text text-info"><?= $item[1] ?></p>
 
                             <?php } ?>
-                        <div class=""></div>
+                            <div class=""></div>
+                        </div>
+
+                        <div class="card-footer">
+                            <?php if (in_array($car['id'], $_SESSION['compareList'])) { ?>
+                                <a class="btn btn-success d-flex" href="/?route=catalog&delcompare=<?= $car['id'] ?>">Remove from compare</a>
+                            <?php } ?>
+                        </div>
                     </div>
-                </div>
                 <?php } ?>
             </div>
         </div>
