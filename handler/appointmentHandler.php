e47d6e11a96d6775cc11e5d352bcb6a4c44a73b6
AkitoShing <samuelxx100799@gmail.com>
2020-07-08 01:44:12 +0800

    Before i die

---
+++ handler/appointmentHandler.php
@@ -0,0 +1,10 @@
+<?php
+if (isset($_GET['make'])){
+    
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (isset($_POST['action'])){
+        die('you father fucker');
+    }
+}
--- sessionHelper.php
+++ sessionHelper.php
@@ -4,8 +4,9 @@ if (isset($_GET['action'])){
     switch ($_GET['action']){
         case 'destroy': session_destroy(); break;
         case 'get': echo ("<pre>"); var_dump($_SESSION[$_GET['key']]); echo("</pre>"); break;
-        case 'set': $_SESSION[$_GET['key']] = $_GET['value']; echo ("<pre>"); var_dump($_SESSION); echo("</pre>"); break;
-        case 'unset': unset($_SESSION[$_GET['key']]); echo ("<pre>"); var_dump($_SESSION); echo("</pre>"); break;
-        case 'dd': echo ("<pre>"); var_dump($_SESSION); echo("</pre>"); break;
+        case 'set': $_SESSION[$_GET['key']] = $_GET['value']; break;
+        case 'unset': unset($_SESSION[$_GET['key']]); break;
+        case 'login': $_SESSION['customer']['id'] = 'C000001'; break;
     }
+    echo ("<pre>"); var_dump($_SESSION); echo("</pre>");
 }
