16b22d479af7591754f4da5b1907137472ee075a
AkitoShing <samuelxx100799@gmail.com>
2020-07-07 21:12:02 +0800

    update session helper

--- sessionHelper.php
+++ sessionHelper.php
@@ -3,6 +3,9 @@ session_start();
 if (isset($_GET['action'])){
     switch ($_GET['action']){
         case 'destroy': session_destroy(); break;
+        case 'get': echo ("<pre>"); var_dump($_SESSION[$_GET['key']]); echo("</pre>"); break;
+        case 'set': $_SESSION[$_GET['key']] = $_GET['value']; echo ("<pre>"); var_dump($_SESSION); echo("</pre>"); break;
+        case 'unset': unset($_SESSION[$_GET['key']]); echo ("<pre>"); var_dump($_SESSION); echo("</pre>"); break;
         case 'dd': echo ("<pre>"); var_dump($_SESSION); echo("</pre>"); break;
     }
 }
