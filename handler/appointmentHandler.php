72a7fd70afac156ad698cd86a250fb4d7ee0fd0f
AkitoShing <samuelxx100799@gmail.com>
2020-07-09 01:02:39 +0800

    Update catalog, appointment and compare

--- handler/appointmentHandler.php
+++ handler/appointmentHandler.php
@@ -1,13 +1,42 @@
 <?php
 session_start();
-require_once '../DBFunctions.php';
+require_once "../DBFunctions.php";
+
+if (!isset($_SESSION['customer']['id'])) { ?>
+    <script>
+        alert('Making appointments require login!');
+        window.location.replace(window.location.origin + '/?route=login');
+    </script>
+<?php }
 
 if ($_SERVER['REQUEST_METHOD'] == "POST") {
-    if (isset($_POST['action'])){
-//        die("i m drunk");
-        dd(insertPrepare('appointments',
-            ['carId', 'appointmentUserId'],
-            [$_POST['carId'], $_SESSION['customer']['id']]
-        ));
+    if (isset($_POST['action']) && $_POST['action'] === 'make') {
+        if (isset($_POST['carId'], $_POST['appointmentDateTime'], $_POST['appointmentNotes'])) {
+            if (queryBuilderPrepare('cars', ['id'], ['id' => $_POST['carId']]) != null) {
+                createAppointment($_POST['carId'], datetime_localToTimestamp($_POST['appointmentDateTime']), $_POST['appointmentNotes']); ?>
+                <script>
+                    alert('Appointment made!');
+                    window.location.replace(window.location.origin + '/?route=catalog');
+                </script>
+            <?php } else { ?>
+                <script>
+                    alert('no such car existed');
+                    window.location.replace(window.location.origin + '/?route=catalog');
+                </script>
+            <?php }
+        }
     }
+}
+
+function createAppointment($carId, $appointmentDateTime, $appointNotes)
+{
+    insertPrepare('appointments',
+        ['carId', 'appointmentUserId', 'appointmentDateTime', 'appointmentNotes'],
+        [$_POST['carId'], $_SESSION['customer']['id'], datetime_localToTimestamp($appointmentDateTime), $appointNotes]
+    );
+}
+
+function datetime_localToTimestamp($datetime)
+{
+    return gmdate('Y-m-d H:i:s', strtotime($datetime));
 }
--- page/makeAppointment.php
+++ page/makeAppointment.php
@@ -7,55 +7,61 @@ if (!isset($_SESSION['customer']['id'])) {
 }
 if (isset($_GET['carId'])) {
     $car = queryBuilderPrepare('cars',
-        ['cars.name as carName', 'customers.name as customerName', 'cars.description', 'cars.photoLocation', 'cars.id as carId'],
+        ['cars.name as carName', 'customers.name as customerName', 'cars.description', 'cars.photoLocation', 'cars.id as carId', 'customers.name as ownerName'],
         ['cars.id' => $_GET['carId']],
 //        ['carId'=>$_GET['carId'], 'customerId'=>$_GET['customerId']],
         [],
         [['customers', 'cars.ownerId', 'customers.id']]
     )[0];
-    dd($car);
 }
 
 ?>
 <div class="container">
     <div class="card">
-        <div class="card-header">
-            <h2>Appointment Form</h2>
-        </div>
-        <div class="row">
-            <div class="col-6">
-                <div class="card-body">
-                    <img class="card-img" src="<?=$car['photoLocation']?>" alt="" style="height: 320px; width: 500px;">
+        <form action="../handler/appointmentHandler.php" method="post">
+            <div class="card-header">
+                <h2>Appointment Form</h2>
+            </div>
+            <div class="row">
+                <div class="col-6">
                     <div class="card-body">
-                        <h3 class="font-weight-bold "><?= $car['carName'] ?></h3>
-                        <p><?= $car['description'] ?></p>
+                        <img class="card-img" src="<?= $car['photoLocation'] ?>" alt=""
+                             style="height: 320px; width: 500px;">
+                        <div class="card-body">
+                            <h3 class="font-weight-bold "><?= $car['carName'] ?></h3>
+                            <p><?= $car['description'] ?></p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="card-body">
+                        <h4>Appointment detail</h4>
+                        <div class="form-group">
+                            <label for="ownerName">Owner Name: </label>
+                            <input type="text" class="form-control" name="ownerName" id="ownerName"
+                                   value="<?= $car['ownerName'] ?>" disabled>
+                        </div>
+                        <div class="form-group">
+                            <label for="appointmentDateTime">Meeting Data and Time:</label>
+                            <input type="datetime-local" class="form-control" name="appointmentDateTime"
+                                   id="appointmentDateTime" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="appointmentNotes">Note to owner</label>
+                            <textarea class="form-control" name="appointmentNotes" id="appointmentNotes" cols="30"
+                                      rows="8"></textarea>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="col-6">
-                <div class="card-body">
-                    <h3 class="my-3">Project Description</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida
-                        pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus.
-                        Mauris ultricies, justo eu convallis placerat, felis enim.</p>
-                    <h3 class="my-3">Project Details</h3>
-                    <ul>
-                        <li>Lorem Ipsum</li>
-                        <li>Dolor Sit Amet</li>
-                        <li>Consectetur</li>
-                        <li>Adipiscing Elit</li>
-                    </ul>
+            <div class="card-footer">
+                <input type="hidden" name="action" value="make">
+                <input type="hidden" name="carId" value="<?= $car['carId'] ?>">
+                <div class="form-group">
+                    <button class="btn btn-danger form-control">Confirm book appointment</button>
+
                 </div>
             </div>
-        </div>
-        <div class="card-footer">
-            <form action="../handler/appointmentHandler.php" method="post">
-                <input type="hidden" name="action" value="make">
-                <input type="hidden" name="carId" value="<?=$car['carId']?>">
-                <button class="btn btn-danger">Confirm book appointment</button>
-            </form>
-        </div>
-        <!-- /.row -->
-
+        </form>
     </div>
 </div>
