52e78bcb861a530d1b84f29361d61ef07961a6b1
holiday526 <taylorchan526@gmail.com>
2020-07-10 01:29:50 +0800

    update search

--- page/content.php
+++ page/content.php
@@ -1,7 +1,12 @@
 <div class="container">
 
 <?php
-dd(queryBuilderPrepare('admins', ['*']));
+dd(queryBuilderPrepare('cars',
+    ['cars.name as carName', 'cars.id as carId', 'customers.id as customerId'],
+    ['cars.id'=>$_GET['carId']],
+    [],
+    [['customers', 'cars.ownerId', 'customers.id']]
+));
 ?>
 
 </div>
