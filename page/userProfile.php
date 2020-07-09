377e6f9366d362cd41bd7fb23e2e7b3d6d058f23
AkitoShing <samuelxx100799@gmail.com>
2020-07-10 02:32:14 +0800

    Update userProfile all input is disabled form action, method remove remove submit button

--- page/userProfile.php
+++ page/userProfile.php
@@ -9,7 +9,7 @@ session_start();
 
 <div class="container mt-4">
     <h4>Customer Register</h4>
-    <form action="../handler/userProfileHandler.php" method="post">
+    <form action="#">
         <div class="form-group">
             <label for="emailInput">Email address</label>
             <input type="email" name="email" class="form-control" id="emailInput" value="<?= $user['email'] ?>"
@@ -25,7 +25,7 @@ session_start();
         <div class="form-group">
             <label for="customerNameInput">Customer Name</label>
             <input type="text" name="customerName" class="form-control" id="customerNameInput"
-                   value="<?= $user['name'] ?>" placeholder="Enter your name" required>
+                   value="<?= $user['name'] ?>" placeholder="Enter your name" disabled>
         </div>
         <div class="form-group">
             <label for="passwordInput">Password</label>
@@ -43,20 +43,20 @@ session_start();
                 characters.</small>
         </div>
         <div class="form-group form-check">
-            <input type="checkbox" class="form-check-input" id="corporateCustomerCheckbox" name="corporateCustomer">
+            <input type="checkbox" class="form-check-input" id="corporateCustomerCheckbox" name="corporateCustomer" disabled>
             <label class="form-check-label" for="corporateCustomerCheckbox">Corporate Customer?</label>
         </div>
         <div class="form-group">
             <label for="addressTextArea">Address</label>
             <textarea class="form-control" id="addressTextArea" name="address" rows="3"
-                      required><?= $user['address'] ?></textarea>
+                      disabled><?= $user['address'] ?></textarea>
         </div>
         <div class="form-group">
             <label for="contactPhones">Contact Phones</label>
             <div class="input-group mb-2">
                 <?php foreach (explode(',', $user['contactPhones']) as $phone) { ?>
                     <input type="text" class="form-control" value="<?=$phone?>" placeholder="Phone No." name="contactPhones[]"
-                           id="contactPhones" required>
+                           id="contactPhones" disabled>
                 <?php } ?>
                 <div class="input-group-append">
                     <button class="btn btn-secondary" type="button" onclick="createNewContactPhoneInput()">+</button>
@@ -65,9 +65,6 @@ session_start();
             <div id="newPhoneContactInput"></div>
         </div>
         <div class="form-group form-row">
-            <div class="col-4 mx-auto">
-                <input type="submit" class="btn btn-success btn-block">
-            </div>
             <div class="col-4 mx-auto">
                 <a href="?route=logout" class="btn btn-primary btn-md btn-block">Logout Current</a>
             </div>
