447778f7d3083f5c6bb7f38d6f20af203cc8d43d
mlml2145 <67783485+mlml2145@users.noreply.github.com>
2020-07-13 00:18:06 +0800

    rename title

--- layout/head.php
+++ layout/head.php
@@ -3,7 +3,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Modern Business - Start Bootstrap Template</title>
+    <title>Auto Car Sales Ltd</title>
 
     <!-- Bootstrap core CSS -->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
